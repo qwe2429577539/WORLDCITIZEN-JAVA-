b1328f7ae13ed1e0f65fddae28f00cdc044428b8
WhatFishBoat <zhourenyu1994@hotmail.com>
2022-09-16 23:21:07 +0800

    Now it can send message and display in the screen immediately. also added a few new features.

--- app_flutter_01/flutter_application_1/lib/subpage/contact.dart
+++ app_flutter_01/flutter_application_1/lib/subpage/contact.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/mainpage/chat.dart';
 
@@ -8,6 +6,8 @@ const sendMessageBubbleColor = Color.fromARGB(255, 255, 249, 243);
 const receiveMessageBubbleColor = Color.fromARGB(255, 108, 95, 188);
 const sendMessageColor = Color.fromARGB(255, 42, 46, 83);
 const receiveMessageColor = Color.fromARGB(255, 255, 255, 255);
+const inputCursorColor = Color.fromARGB(255, 155, 139, 139);
+const messageBarColor = Color.fromARGB(255, 255, 255, 255);
 
 /// 聊天背景颜色设置
 const gradientDecorationContact = BoxDecoration(
@@ -28,40 +28,52 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPage extends State<ContactPage> {
+  /// TextField Controller
   final TextEditingController _textController = TextEditingController();
-  List<Widget> bubbleList = <Widget>[];
 
+  /// ListView.Builder's scroll controller
+  final ScrollController _scrollController = ScrollController();
+
+  /// 判断TextField内是否为空消息，空消息则不可sendMessage
+  bool _emptyMessage = true;
+
+  /// 临时储存聊天信息的容器
+  List textList = [];
+
+  /// 获取TextField内信息，并存入容器
   void getInputText() {
+    String text = _textController.text;
+    textList.add([text, true]);
+  }
+
+  /// 发出信息
+  void sendMessage() {
+    getInputText();
+    _textController.clear();
     setState(() {
-      String text = _textController.text;
-      _textController.clear();
-      // bubbleList.add(contactBubbleOutside(text, true));
-      bubbleList.add(contactBubble(text, true));
+      _emptyMessage = true;
+    });
+    // 控制跳转到最新消息,需要delay来等待maxScrollExtent更新
+    // Reference: https://blog.csdn.net/adojayfan/article/details/120290662
+    Future.delayed(Duration(milliseconds: 100), () {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
     });
   }
 
-  /// 聊天信息陈列
-  Widget displayMessage() {
+  /// 聊天信息陈列控件
+  Widget displayMessage(List textList) {
     return Container(
         decoration: gradientDecorationContact,
         child: Padding(
             padding:
                 const EdgeInsets.only(left: 18, right: 18, top: 15, bottom: 0),
             child: ListView.builder(
-                itemCount: bubbleList.length,
+                controller: _scrollController,
+                itemCount: textList.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return bubbleList[index];
-                }))
-        // child: ListView(
-        //   children: bubbleList,
-        //   //   children: [
-        //   //   contactBubble("Hello!", false),
-        //   //   contactBubble("Greeting!", true),
-        //   // ]
-        //   ),
-        // ),
-        );
-  } // 聊天信息陈列
+                  return contactBubble(textList[index][0], textList[index][1]);
+                })));
+  }
 
   IconButton emojiButton() {
     return IconButton(
@@ -70,33 +82,34 @@ class _ContactPage extends State<ContactPage> {
     );
   }
 
-  IconButton sendButton() {
-    return IconButton(
-      onPressed: () {
-        setState(() {
-          getInputText();
-        });
-      },
-      icon: const Icon(Icons.send),
-    );
-  }
-
   IconButton infoButton() {
-    String text;
     return IconButton(
       onPressed: null,
       icon: Icon(Icons.info),
     );
   }
 
+  IconButton sendButton() {
+    return IconButton(
+      onPressed: _emptyMessage ? null : () => sendMessage(),
+      icon: const Icon(Icons.send),
+    );
+  }
+
+  /// 输入框控件
   Widget inputField() {
     return Flexible(
-        //color: Color.fromARGB(255, 168, 139, 139),
         child: TextField(
       controller: _textController,
-      onChanged: (value) {},
-      onSubmitted: (value) {},
-      cursorColor: const Color.fromARGB(255, 155, 139, 139),
+      onChanged: (String text) {
+        setState(() {
+          _emptyMessage = text.isEmpty;
+        });
+      },
+      onSubmitted: (String text) {
+        sendMessage;
+      },
+      cursorColor: inputCursorColor,
       maxLength: null,
       maxLines: null,
       decoration: const InputDecoration.collapsed(hintText: "Type a message"),
@@ -105,7 +118,7 @@ class _ContactPage extends State<ContactPage> {
 
   Widget messageBar() {
     return Container(
-      color: const Color.fromARGB(255, 255, 255, 255),
+      color: messageBarColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -118,13 +131,59 @@ class _ContactPage extends State<ContactPage> {
     );
   }
 
+  /// 聊天气泡控件 <br>
+  /// sentText (String): 气泡消息内容 <br>
+  /// messageType (boolean): 消息来源开关,
+  ///                        true为己方发出,false为对方发出，控制气泡位置、颜色等
+  Widget contactBubble(sentText, messageType) {
+    const radius = Radius.circular(18);
+    const angle = Radius.circular(0);
+
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment:
+          messageType ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 250),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            margin: const EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+                color: messageType
+                    ? receiveMessageBubbleColor
+                    : sendMessageBubbleColor,
+                borderRadius: BorderRadius.only(
+                  topRight: messageType ? angle : radius,
+                  topLeft: messageType ? radius : angle,
+                  bottomLeft: radius,
+                  bottomRight: radius,
+                )),
+            child: Text(
+              sentText,
+              style: TextStyle(
+                  color: messageType ? receiveMessageColor : sendMessageColor),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 设置初始化state，暂用于展示对方聊天气泡
+  @override
+  void initState() {
+    super.initState();
+    // 用于演示
+    // 展示对方聊天气泡
+    textList.add(["Hello!", true]);
+    textList.add(["Greeting!", false]);
+    textList.add(["How's going?", false]);
+  }
+
+  /// build the Widget
   @override
   Widget build(BuildContext context) {
-    bubbleList = <Widget>[
-      contactBubble("Hello!", true),
-      contactBubble("Greeting!", false),
-    ];
-
     return Container(
       decoration: gradientDecoration,
       child: Scaffold(
@@ -151,11 +210,11 @@ class _ContactPage extends State<ContactPage> {
               Expanded(
                 child: Column(
                   children: [
-                    // 聊天窗口
+                    // 聊天显示窗口
                     Expanded(
                       flex: 8,
-                      child: displayMessage(),
-                    ), // 聊天窗口
+                      child: displayMessage(textList),
+                    ), // 聊天显示窗口
 
                     //输入栏
                     Expanded(
@@ -171,53 +230,4 @@ class _ContactPage extends State<ContactPage> {
       ),
     );
   }
-
-  /// 聊天气泡组件
-  /// sentText (String): 气泡消息内容
-  /// messageType (boolean): 消息来源开关,true为己方发出,false为对方发出，控制气泡位置、颜色等
-  Widget contactBubble(sentText, messageType) {
-    const radius = Radius.circular(18);
-    const angle = Radius.circular(0);
-
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment:
-          messageType ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(15),
-          margin: const EdgeInsets.only(top: 10),
-          decoration: BoxDecoration(
-              color: messageType
-                  ? receiveMessageBubbleColor
-                  : sendMessageBubbleColor,
-              borderRadius: BorderRadius.only(
-                topRight: messageType ? angle : radius,
-                topLeft: messageType ? radius : angle,
-                bottomLeft: radius,
-                bottomRight: radius,
-              )),
-          child: Text(
-            sentText,
-            style: TextStyle(
-                color: messageType ? receiveMessageColor : sendMessageColor),
-          ),
-        )
-      ],
-    );
-  } // 聊天气泡组件
 }
-
-// class Display extends StatefulWidget {
-//   const Display({Key? key}) : super(key: key);
-
-//   @override
-//   State<Display> createState() => _Display();
-// }
-
-// class _Display extends State<Display> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return 
-//   }
-// }
